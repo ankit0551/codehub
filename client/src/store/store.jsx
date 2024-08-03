dc6031267073d277a5dc13885f43402617342005
Ankit Kumar <ankitraj0551@gmail.com>
2024-08-03 20:24:23 +0530

    updated title

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -6,9 +6,11 @@ const useStore = create((set) => ({
     user : curruser ?  JSON.parse(curruser) : null,
     token : "",
     chat : [],
+    loading : false,
     setUser : (newUser) => set({user: newUser}),
     setToken : (newToken) => set({token : newToken}),
     setChat : (newChat) => set((state)=> ({chat : [...state.chat, newChat]})),
+    setLoading : (newLoading) => set({loading : newLoading}),
 }))
 
 export default useStore;
